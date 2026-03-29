bf004e4ac71153be563aa2fe8f9ce5b28fcee446
DhandapaniSakthivel <98797379+DhandapaniSakthivel@users.noreply.github.com>
2026-03-29 20:31:53 +0530

    Create ProductRepository.java

---
+++ src/main/java/com/example/myapp/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.example.myapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.myapp.model.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
